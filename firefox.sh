86250f0f2c67d82c2db98a57ae788b67cfe0faa7
mobilturka <93243536+mobilturka@users.noreply.github.com>
2023-09-14 15:15:20 +0300

    Update firefox.sh

--- firefox.sh
+++ firefox.sh
@@ -11,13 +11,9 @@ cat >> $HOME/.local/share/applications/Firefox.desktop << EOF
 [Desktop Entry]
 Version=1.0
 Name=Firefox
-Name[tr]=Firefox
-Comment=Browse the World Wide Web
-Comment[tr]=İnternet'te Gezinin
-GenericName=Web Browser
-GenericName[tr]=Web Tarayıcı
+Comment=İnternet'te Gezinin
+GenericName=Web Tarayıcı
 Keywords=Internet;WWW;Browser;Web;Explorer
-Keywords[tr]=İnternet;WWW;Tarayıcı;Web;Gezgin;Web sitesi;Site;sörf;çevrimiçi;tara
 Exec=firefox
 Terminal=false
 X-MultipleArgs=false
@@ -26,21 +22,8 @@ Icon=$HOME/.local/firefox/browser/chrome/icons/default/default128.png
 Categories=GNOME;GTK;Network;WebBrowser;
 MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/ftp;x-scheme-handler/chrome;video/webm;application/x-xpinstall;
 StartupNotify=true
-Actions=NewWindow;NewPrivateWindow;
-
-[Desktop Action NewWindow]
-Name=Open a New Window
-Name[tr]=Yeni pencere aç 
-Exec=firefox -new-window
-OnlyShowIn=Unity;
-
-[Desktop Action NewPrivateWindow]
-Name=Open a New Private Window
-Name[tr]=Yeni gizli pencere aç
-Exec=firefox -private-window
-OnlyShowIn=Unity;
 EOF
-tar -xvf /tmp/firefox.tar.bz2 -C ~/.local
+tar -xjf /tmp/firefox.tar.bz2 -C ~/.local
 
 sudo apt install libdbus-glib-1-2
 sudo ln -s $HOME/.local/firefox/firefox /usr/bin/firefox
